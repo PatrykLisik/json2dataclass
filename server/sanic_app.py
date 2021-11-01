d17ccb647dadeff9d195be22c1a18fc7f1f06ebf
Patryk Lisik <patrykkbkak@gmail.com>
2021-11-01 07:58:24 +0100

    Revert "Feature/json to dataclass (#2)"
    This reverts commit 8be75096f614b3a08315c1276c2d3d65a22c991f.

--- server/sanic_app.py
+++ server/sanic_app.py
@@ -1,18 +1,12 @@
 from sanic import Sanic
-from sanic.response import text, json
-
-from models.dataclass_model import PythonDataModel
+from sanic.response import text
 
 app = Sanic("Json2Dataclass")
 
 
-@app.post("/api/to_dataclass")
-async def to_dataclass(request):
-    to_parse = request.json["json_to_convert"]
-    mapped_model = PythonDataModel(to_parse).generate_code()
-    return json({
-        "dataclass": mapped_model
-    })
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world")
 
 
 if __name__ == "__main__":
