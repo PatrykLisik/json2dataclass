e0855a6700cdb37998b5b0bc88efeea908cb37db
Patryk Lisik <lisikpatryk@gmail.com>
2022-02-05 19:36:03 +0100

    button with request works

--- json2dataclass.py
+++ json2dataclass.py
@@ -1,19 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, abort
 
 from .models.dataclass_model import PythonDataModel
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="")
 
 
 @app.post("/api/to_dataclass")
 def to_dataclass():
-    to_parse = request.json["json_to_convert"]
-    mapped_model = PythonDataModel(to_parse).generate_code()
-    return {
-        "dataclass": mapped_model
-    }
+    try:
+        to_parse = request.json["json_to_convert"]
+        mapped_model = PythonDataModel(to_parse).generate_code()
+        return {
+            "dataclass": mapped_model
+        }
+    except Exception as e:
+        app.logger.error(f"Request payload {request.json}")
+        app.logger.error(e)
+        abort(500, "Internal Error")
 
 
 @app.get("/")
 def index():
-    return "hello"
+    return render_template('index.html')
