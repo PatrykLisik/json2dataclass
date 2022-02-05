52de67593aa1d21ff0dc4faac9960ba2f65541a0
Patryk Lisik <lisikpatryk@gmail.com>
2022-02-05 20:43:46 +0100

    frontend works a little

--- json2dataclass.py
+++ json2dataclass.py
@@ -8,13 +8,13 @@ app = Flask(__name__, static_folder="")
 @app.post("/api/to_dataclass")
 def to_dataclass():
     try:
+        app.logger.info(f"Request payload {request.json}")
         to_parse = request.json["json_to_convert"]
         mapped_model = PythonDataModel(to_parse).generate_code()
         return {
             "dataclass": mapped_model
         }
     except Exception as e:
-        app.logger.error(f"Request payload {request.json}")
         app.logger.error(e)
         abort(500, "Internal Error")
 
