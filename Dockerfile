e22ed570e7677e347148020a7c11233559894132
Patryk Lisik <lisikpatryk@gmail.com>
2022-02-05 20:49:42 +0100

    Feature/bootstrap frontend (#7)
    * docker-compose.yml adjusted to new structure
    
    * moved to flask
    
    * button with request works
    
    * heroku file adjusted to flask
    
    * import in test adjusted
    
    * frontend works a little
    
    * Add .circleci/config.yml (#6)
    
    * Add .circleci/config.yml
    
    * upgrade python version
    
    * relative import in test

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM python:3.9-slim
 WORKDIR /app
 
 COPY . /app
+ENV FLASK_APP=json2dataclass:app
 RUN pip install -r requirements.txt
---
+++ json2dataclass.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, abort
+
+from .models.dataclass_model import PythonDataModel
+
+app = Flask(__name__, static_folder="")
+
+
+@app.post("/api/to_dataclass")
+def to_dataclass():
+    try:
+        app.logger.info(f"Request payload {request.json}")
+        to_parse = request.json["json_to_convert"]
+        mapped_model = PythonDataModel(to_parse).generate_code()
+        return {
+            "dataclass": mapped_model
+        }
+    except Exception as e:
+        app.logger.error(e)
+        abort(500, "Internal Error")
+
+
+@app.get("/")
+def index():
+    return render_template('index.html')
